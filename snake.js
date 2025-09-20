e81cf407c723ae809a9f3ad5ff125d0a8ad07edc
Sarthak Upadhyay <u.sarthak2005@gmail.com>
2025-09-20 12:29:42 +0530

    adding movement to the snake head using event

--- snake.js
+++ snake.js
@@ -1,3 +1,40 @@
 let canvas = document.getElementById('canvas');
 let cxt = canvas.getContext('2d')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let snake = {
+    x: 100,
+    y: 100,
+    dx: 0,
+    dy: 0
+}
+window.addEventListener('keydown',(e)=>{
+  if(e.key == "ArrowUp") {
+    snake.dy = -2;
+  }
+  if(e.key == "ArrowDown") {
+   snake.dy = 2;
+  }
+  if(e.key == "ArrowLeft") {
+    snake.dx = -2; 
+  }
+  if(e.key == "ArrowRight") {
+    snake.dx = 2; 
+  }
+});
 
+
+window.addEventListener('resize',()=>{canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;})
+
+
+function animate(){ 
+    cxt.clearRect(0,0,canvas.width,canvas.height)
+    cxt.fillStyle = "green"
+    snake.x += snake.dx;
+    snake.y += snake.dy;
+    cxt.fillRect(snake.x,snake.y,20,20)
+requestAnimationFrame(animate)
+}
+
+animate();
